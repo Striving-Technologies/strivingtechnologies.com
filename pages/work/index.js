2315c47cde037d756665d2e1aa8b55de2e8881cf
deltanboi <pedroogheneochuko@gmail.com>
2022-02-02 00:03:22 +0100

    Finish stritech project pages

--- pages/work/index.js
+++ pages/work/index.js
@@ -1,19 +1,19 @@
-import React, { useEffect } from 'react';
-import Head from 'next/head';
-import Link from 'next/link';
+import React, { useEffect } from "react";
+import Head from "next/head";
+import Link from "next/link";
 
-import { Button } from 'antd';
-import { ArrowUpOutlined, ArrowDownOutlined } from '@ant-design/icons';
+import { Button } from "antd";
+import { ArrowUpOutlined, ArrowDownOutlined } from "@ant-design/icons";
 
-import MainLayout from '../../components/mainlayout/mainlayout';
+import MainLayout from "../../components/mainlayout/mainlayout";
 
-import styles from '../../static/css/work.module.scss';
+import styles from "../../static/css/work.module.scss";
 
 export default function work({ workList }) {
   let scrollToComponent;
   useEffect(() => {
-    const isBrowser = typeof document !== 'undefined';
-    const AOS = isBrowser ? require('aos') : undefined;
+    const isBrowser = typeof document !== "undefined";
+    const AOS = isBrowser ? require("aos") : undefined;
 
     AOS.init({
       // disable: 'mobile',
@@ -21,7 +21,7 @@ export default function work({ workList }) {
       mirror: true,
       duration: 1000,
     });
-    scrollToComponent = require('react-scroll-to-component');
+    scrollToComponent = require("react-scroll-to-component");
   }, []);
   return (
     <MainLayout>
@@ -42,32 +42,32 @@ export default function work({ workList }) {
             {workList.map((work, index, worklist) => {
               return (
                 <div
-                  className={[styles.block, styles.caseCont].join(' ')}
+                  className={[styles.block, styles.caseCont].join(" ")}
                   key={work.id + index}
                   style={{
                     color: work.color,
                     backgroundColor: work.backgroundColor,
-                    height: '100vh',
-                    width: '100vw',
-                    maxWidth: '100vw',
+                    height: "100vh",
+                    width: "100vw",
+                    maxWidth: "100vw",
                   }}
                   ref={(div) => {
                     work.id = div;
                   }}
                 >
-                  <span style={{ height: '30px' }}>
+                  <span style={{ height: "30px" }}>
                     {index !== 0 && (
                       <Button
-                        shape='circle'
-                        type='ghost'
-                        style={{ borderColor: '#f9f9f9' }}
+                        shape="circle"
+                        type="ghost"
+                        style={{ borderColor: "#f9f9f9" }}
                         className={styles.move}
-                        icon={<ArrowUpOutlined style={{ color: '#f9f9f9' }} />}
+                        icon={<ArrowUpOutlined style={{ color: "#f9f9f9" }} />}
                         onClick={() => {
                           if (worklist[index - 1])
                             scrollToComponent(worklist[index - 1].id, {
                               offset: 0,
-                              align: 'top',
+                              align: "top",
                               duration: 500,
                             });
                         }}
@@ -75,31 +75,33 @@ export default function work({ workList }) {
                     )}
                   </span>
                   <div className={styles.case}>
-                    <div data-aos='fade-up'>
+                    <div data-aos="fade-up">
                       <h1>{work.title}</h1>
-                      <p>{work.clip}</p>
+                      <p style={{ fontSize: 20 }}>{work.clip}</p>
                       <Link href={`/work/${work.slug}`}>
                         <a style={{ color: work.color }}>
                           <p>Read Case Study</p>
                         </a>
                       </Link>
-                      <p>{work.tool}</p>
+                      <p style={{ fontSize: 16 }}>{work.tool}</p>
                     </div>
-                    <img data-aos='fade-down' src={`${work.images.full}`} />
+                    <img data-aos="fade-down" src={`${work.images.full}`} />
                   </div>
-                  <span style={{ height: '30px' }}>
+                  <span style={{ height: "30px" }}>
                     {index < worklist.length - 1 && (
                       <Button
-                        shape='circle'
-                        type='ghost'
-                        style={{ borderColor: '#f9f9f9' }}
+                        shape="circle"
+                        type="ghost"
+                        style={{ borderColor: "#f9f9f9" }}
                         className={styles.move}
-                        icon={<ArrowDownOutlined style={{ color: '#f9f9f9' }} />}
+                        icon={
+                          <ArrowDownOutlined style={{ color: "#f9f9f9" }} />
+                        }
                         onClick={() => {
                           if (worklist[index + 1])
                             scrollToComponent(worklist[index + 1].id, {
                               offset: 0,
-                              align: 'top',
+                              align: "top",
                               duration: 500,
                             });
                         }}
