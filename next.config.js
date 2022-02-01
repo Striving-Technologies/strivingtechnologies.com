8c147314c497558e0b5098e319092bd46bb73020
deltanboi <pedroogheneochuko@gmail.com>
2022-02-01 17:29:56 +0100

    Configure webpack 5
    Please work

--- next.config.js
+++ next.config.js
@@ -1,9 +1,22 @@
+// module.exports = {
+//   webpack: config => {
+//     // Fixes npm packages that depend on `fs` module
+//     config.node = {
+//       fs: 'empty'
+//     }
+//     return config
+//   }
+// }
+
 module.exports = {
-  webpack: config => {
-    // Fixes npm packages that depend on `fs` module
+  webpack5: false,
+  webpack: (config) => {
     config.node = {
-      fs: 'empty'
-    }
-    return config
-  }
-}
+      dns: "mock",
+      fs: "empty",
+      path: true,
+      url: false,
+    };
+    return config;
+  },
+};
