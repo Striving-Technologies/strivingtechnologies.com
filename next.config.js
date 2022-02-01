73357f657c162023b5db2ca62ac3db1dd8ca3ccf
deltanboi <pedroogheneochuko@gmail.com>
2022-02-01 17:40:25 +0100

    Just out here configuring webpack

--- next.config.js
+++ next.config.js
@@ -8,15 +8,22 @@
 //   }
 // }
 
+// module.exports = {
+//   webpack5: false,
+//   webpack: (config) => {
+//     config.node = {
+//       dns: "mock",
+//       fs: "empty",
+//       path: true,
+//       url: false,
+//     };
+//     return config;
+//   },
+// };
+
 module.exports = {
-  webpack5: false,
-  webpack: (config) => {
-    config.node = {
-      dns: "mock",
-      fs: "empty",
-      path: true,
-      url: false,
-    };
+  webpack: (config, { isServer }) => {
+    if (!isServer) config.resolve.fallback.fs = false;
     return config;
   },
 };
